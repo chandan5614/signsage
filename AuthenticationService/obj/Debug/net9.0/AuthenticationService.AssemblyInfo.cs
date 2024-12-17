48d00eb55da64098b4b838d7664676f97ac37b20
chandan5614 <chandan.dy1995@gmail.com>
2024-12-17 15:51:39 -0600

    Add gitignore file

--- AuthenticationService/obj/Debug/net9.0/AuthenticationService.AssemblyInfo.cs
+++ AuthenticationService/obj/Debug/net9.0/AuthenticationService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthenticationService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3f7eb3ad59efa93cc05a3bb4cd9ad0417f682aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthenticationService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthenticationService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
