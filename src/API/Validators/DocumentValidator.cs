8aa12038b0abb7bd5e91b0dba3a0d1d08bd48092
chandan5614 <chandan.dy1995@gmail.com>
2024-12-18 00:42:04 -0600

    Add validators for Document and Signature DTOs

--- src/API/Validators/DocumentValidator.cs
+++ src/API/Validators/DocumentValidator.cs
@@ -1,12 +1,21 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using FluentValidation;
+using API.DTOs.Document;
 
 namespace API.Validators
 {
-    public class DocumentValidator
+    public class DocumentValidator : AbstractValidator<CreateDocumentDto>
     {
-        
+        public DocumentValidator()
+        {
+            RuleFor(d => d.Title)
+                .NotEmpty().WithMessage("Title is required.")
+                .MaximumLength(200).WithMessage("Title must not exceed 200 characters.");
+
+            RuleFor(d => d.Description)
+                .MaximumLength(500).WithMessage("Description must not exceed 500 characters.");
+
+            RuleFor(d => d.UserId)
+                .NotEmpty().WithMessage("UserId is required.");
+        }
     }
 }
--- src/API/Validators/SignatureValidator.cs
+++ src/API/Validators/SignatureValidator.cs
@@ -1,12 +1,20 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using FluentValidation;
+using API.DTOs.Signature;
 
 namespace API.Validators
 {
-    public class SignatureValidator
+    public class SignatureValidator : AbstractValidator<CreateSignatureDto>
     {
-        
+        public SignatureValidator()
+        {
+            RuleFor(s => s.SignatureContent)
+                .NotEmpty().WithMessage("Signature content is required.");
+
+            RuleFor(s => s.DocumentId)
+                .NotEmpty().WithMessage("DocumentId is required.");
+
+            RuleFor(s => s.UserId)
+                .NotEmpty().WithMessage("UserId is required.");
+        }
     }
 }
