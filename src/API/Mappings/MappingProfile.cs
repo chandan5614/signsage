83bd376b2db9157a427344e5d008a72ab1395417
chandan5614 <chandan.dy1995@gmail.com>
2024-12-18 00:44:43 -0600

    Add mappings for DTOs and entities

--- src/API/Mappings/MappingProfile.cs
+++ src/API/Mappings/MappingProfile.cs
@@ -1,12 +1,56 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using AutoMapper;
+using API.DTOs.AuditTrail;
+using API.DTOs.Customer;
+using API.DTOs.Document;
+using API.DTOs.Signature;
+using API.DTOs.Template;
+using API.DTOs.User;
+using Core.Entities;
 
 namespace API.Mappings
 {
-    public class MappingProfile
+    public class MappingProfile : Profile
     {
-        
+        public MappingProfile()
+        {
+            // AuditTrail mappings
+            CreateMap<AuditTrail, AuditTrailDto>();
+            CreateMap<AuditTrailDto, AuditTrail>();
+            CreateMap<CreateAuditTrailDto, AuditTrail>();
+
+            // Customer mappings
+            CreateMap<Customer, CustomerDto>();
+            CreateMap<CustomerDto, Customer>();
+            CreateMap<CreateCustomerDto, Customer>();
+            CreateMap<UpdateCustomerDto, Customer>();
+
+            // Document mappings
+            CreateMap<Document, DocumentDto>();
+            CreateMap<DocumentDto, Document>();
+            CreateMap<CreateDocumentDto, Document>();
+            CreateMap<UpdateDocumentDto, Document>();
+
+            // Signature mappings
+            CreateMap<Signature, SignatureDto>();
+            CreateMap<SignatureDto, Signature>();
+            CreateMap<CreateSignatureDto, Signature>();
+
+            // Template mappings
+            CreateMap<Template, TemplateDto>();
+            CreateMap<TemplateDto, Template>();
+            CreateMap<CreateTemplateDto, Template>();
+            CreateMap<UpdateTemplateDto, Template>();
+
+            // User mappings
+            CreateMap<User, UserDto>();
+            CreateMap<UserDto, User>();
+            CreateMap<CreateUserDto, User>();
+            CreateMap<UpdateUserDto, User>();
+
+            // Role mappings
+            CreateMap<Role, RoleDto>();
+            CreateMap<RoleDto, Role>();
+            CreateMap<CreateRoleDto, Role>();
+        }
     }
 }
